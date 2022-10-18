28a17122b759c0da62bbabcfc455db2cc20f615e
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-10-18 23:59:52 +0300

    Add "show more" button functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 import 'focus-visible';
 import onDocumentReady from './helpers/onDocumentReady';
 import lazyImages from './modules/lazyimages';
+import addMoreButton from './modules/addMoreButton';
 
 onDocumentReady(() => {
   lazyImages();
+  addMoreButton();
 });
